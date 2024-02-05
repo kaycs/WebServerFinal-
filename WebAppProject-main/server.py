d3690e235e03d962a3e3d3d73d5eb4887acbec7b
kaylayan <kaylayan@buffalo.edu>
2024-02-05 16:53:40 -0500

    Plan on going to office hours

--- WebAppProject-main/server.py
+++ WebAppProject-main/server.py
@@ -1,13 +1,10 @@
 import socketserver
-from util.request import Request
 # LO2 Goal -
 
 
 #ref : http:// docs.python/org/3/library/socketserver.html?hgihlight=requestthan
 
 
-# Q: Demo from lecture showed the TCP response correct?
-# Q: I Need to parse my request properly to be able to respond?
 class MyTCPHandler(socketserver.BaseRequestHandler):
 # Override Handle method - New TCP connections
     def handle(self):
@@ -15,31 +12,79 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         # we do deal with data
 
         received_data = self.request.recv(2048)
-        # client_id = self.client_address[0] + ":" + str(self_client_address[1])
+
+        #client_id = self.client_address[0] + ":" + str(self_client_address[1])
         #print(client_id + "is sending data:")
         print(len(received_data))
         print(self.client_address)
         print("--- received data ---")
         print(received_data)
         print("--- end of data ---\n\n")
-        request = Request(received_data)
+        #request = Request(received_data)
     #.decode() - string .encode() - bytes
 # CONCATENATE THE RECIEVED DAT YOU GOT - RESPONSE FORMAT
 
-#FIRST PART (STRING) - BODY (BYTE) - CONTENT LENGTH IN BYTES
 
+# HTTP Method + Number Type Message CRLD Content-Length + CRLF + Content-Type + 2X CRFL
+#and message
+
+# How do I access these from my Request class (w/o importing)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------------------------------------------------------------
 # STRING - BYTES
 # request.sendall - response to client
 # LO2 Responses - IMG/HTML/PNG,ETC
 
-    print("\n\n")
-self.request.sendall("HTTP/1.1 Not Found\r\nContent-Length:36\r\nContent-Type text/css; charset=utf-8 \r\n\r\n"
-                     "The requested content does not exist".encode())
+# def ReadingFiles(self):
+#     # Files In Public/ ... (more path )
+#     with open("style.css,style.css", "rb"):
+#         ByteArrCSS = style.css.len()
+#         #CSS Request- /path/public/style.css
+#     #print( # self.request.sendall("POST /path/public/style.css HTTP/1.1 \r\nContent-Length: ____byte arr \r\nX-Content-Type-Options:
+#     #Content-Type: text/css
+#
+#
+#     #HTML request-/ root host
+#     # Q: Can I Convert to Bytes while opening?
+# with open(b'"index.html, index,html"'):
+#     ByteArrHTML = index.html.len()
+
+
+
+
+
+    # self.request.sendall("HTTP/1.1 Not Found\r\nContent-Length:36\r\nContent-Type text/css; charset=utf-8 \r\n\r\n"
+    #                  "The requested content does not exist".encode())
 
 # When data received and processed by request - check path and send app repsone based on path
 # condtional to know what resposne you want - if your path is .... (Threading)
 
-
+#-----------------------------------------------------------------------
 
 
 def __init__(self):
@@ -54,21 +99,10 @@ def __init__(self):
     Server.serve_forever()
 
 
+
+
     # Q: Does this code go into my server - POST request for html/img? (Extending MYTCPHANDLER)
     # nosniff ; charset=utf-8"r\n\r\n")
-    def ReadingFiles(self):
-        # Files In Public/ ... (more path )
-        with open("style.css,style.css", "rb"):
-            ByteArrCSS = style.css.len()
-            #CSS Request- /path/public/style.css
-    #print( # self.request.sendall("POST /path/public/style.css HTTP/1.1 \r\nContent-Length: ____byte arr \r\nX-Content-Type-Options:
-    #Content-Type: text/css
-
-
-    #HTML request-/ root host
-    # Q: Can I Convert to Bytes while opening?
-    with open(b'"index.html, index,html"'):
-        ByteArrHTML = index.html.len()
 
 #print (
 #self.request.sendall("POST / HTTP/1.1 \r\nContent-Length: \r\nX-Content-Type-Options: nosniff ; charset=utf-8" \r\n\r\n")
--- WebAppProject-main/util/request.py
+++ WebAppProject-main/util/request.py
@@ -1,14 +1,4 @@
-class Request:
 
-    def __init__(self, request: bytes):
-        # TODO: parse the bytes of the request and populate the following instance variables
-
-        self.body = b""
-        self.method = ""
-        self.path = ""
-        self.http_version = ""
-        self.headers = {}
-        self.cookies = {}
 class Request:
     # Goal: Parse the HTTP request into it (HTTP) Method, (HTTP) path(including cookies)  ,
     # (HTTP) port,  Headers, and message body (if applicable)
@@ -26,45 +16,54 @@ class Request:
 
 
 
+#    request = Request(b'GET (0)  (1) / (2) HTTP/1.1 (2)
+        #    \r\n (2)
+        #    (1) Host:
+        #    localhost:8080
+        #    \r\n (1)
+        #    (0) Connection: (PART 1) keep-alive (0)
+        #
+        #    \r\n\r\n')
 
 
         #------------------------------------------
         RequestLine = request.split(b"\r\n\r\n")
-        # anfdother CRLF after message body?
         MessageBody = RequestLine[1]
         MessageBody += self.body
         # THE METHOD AND THE PORT HAVE SPACES INBETWEEN THE PATH
         MethnPathPortnHeader = RequestLine[0]
-        HTTPMethnPathPortHeader = MethnPathPortnHeader[0].decode()
-        Headerlines = HTTPMethnPathPortHeader.split("\r\n")
-        HTTPMETHpathjnPort = Headerlines[0]
-        Headerparts = Headerlines[1]
-        Parts = Headerparts[1].split(":")
+        HTTPMethnPathPortHeader = MethnPathPortnHeader.decode()
+        #HTTPMethnPathPortHeader = MethnPathPortnHeader[0].decode()
+        # for all the headers ? - parse? or is it already defualt
+        Headerlines = HTTPMethnPathPortHeader.split("\r\n",2)
+        HTTPMETHpath = Headerlines[2]
+        Headerparts = Headerlines[0]
+        Parts = Headerparts[0].split(":")
         DictHeader = self.headers
         # logic for space/omit - " Content-Length: 5"
-        DictHeader[Parts[0]] = [Parts[1]]
-        HTTPMeth = HTTPMETHpathjnPort.split(" ",2)
-        Method = HTTPMeth[0]
-        Method += self.method
-        # Q: what if my path consist of query_strings(&) or multiple cookies (=) ?  (loop through)- ask still
-        # FIXXXXXXXXXXXXX
-        pathNcookie = HTTPMeth[1]
-        # get the path apart -
-        pathNcookie.split()
-        PaTH = pathNcookie[0]
-        ThePath = self.method
-        Path += ThePath
-        #then split on the cookies
-        Cookies = pathNcookie.split(";")
+        for DictHeader[Parts[0]] in DictHeader:
+            DictHeader[Parts[0]] = DictHeader[Parts[1]]
+            HTTPMeth = HTTPMETHpath.split(" ", 2)
+            Method = HTTPMeth[0]
+            Method += self.method
+            # Q: what if my path consist of query_strings(&) or multiple cookies (=) ?  (loop through)- ask still
+            pathNcookie = HTTPMeth[1]
+            # get the path apart -
+            pathNcookie.split()
+            Path = pathNcookie[1]
+            ThePath = self.method
+            Path += ThePath
+            #then split on the cookies
+            Cookies = pathNcookie.split(";")
             #is there something between the cookies and path?
-        CookieK = Cookies.split("=")
-        Cook = self.cookies
-        Cook[CookieK[0]] = Cook[CookieK[1]]
-            # do you parse the path with or without the cookies?
-        #split on ; then = (through each value) - add to cookies dict
-        Portnum = HTTPMeth[3]
-        Port = self.method
-        Portnum += Port
+            CookieK = Cookies.split("=")
+            Cook = self.cookies
+            Cook[CookieK[0]] = Cook[CookieK[1]]
+             # do you parse the path with or without the cookies?
+            #split on ; then = (through each value) - add to cookies dict
+            Portnum = HTTPMeth[2]
+            Port = self.method
+            Portnum += Port
 
 
 
